5bc5d45a9910f9c5c61531e7140dbb2cd79c8980
ABHILASHA  KUMARI <35931783+461995abhi@users.noreply.github.com>
2018-02-04 00:21:29 +0530

    Add files via upload

---
+++ ledblink123.ino
@@ -0,0 +1,22 @@
+
+
+int i=0;
+int pin=6;
+void setup() {
+
+
+ pinMode(pin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  while(i!=5)
+  {
+
+    digitalWrite(6,HIGH);
+    delay(1000);
+    digitalWrite(6,LOW);
+    i++;
+}
+}
