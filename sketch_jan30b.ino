f946ff64ef20f3290e085ef9baec0d731fb1b2df
ABHILASHA  KUMARI <35931783+461995abhi@users.noreply.github.com>
2018-02-04 00:05:18 +0530

    Add files via upload

---
+++ sketch_jan30b.ino
@@ -0,0 +1,21 @@
+\\\#include<dht.h>
+dht DHT;
+int  pin2=A1;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+Serial.begin(9600);
+pinMode(A1,INPUT);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+DHT.read11(A1);
+Serial.println(DHT.temperature);
+Serial.println(DHT.humidity);
+delay(1000);
+
+}
