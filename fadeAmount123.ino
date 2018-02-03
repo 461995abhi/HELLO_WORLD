242a3c4221d5544fe988788f2067bc4609ff3bc6
ABHILASHA  KUMARI <35931783+461995abhi@users.noreply.github.com>
2018-02-04 00:12:02 +0530

    Add files via upload

---
+++ fadeAmount123.ino
@@ -0,0 +1,25 @@
+
+int led=9;
+int brightness=0;
+int fadeAmount=5;
+
+void setup() {
+  // put your setup code here, to run once:
+
+pinMode(led,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+analogWrite(led,brightness);
+brightness = brightness + fadeAmount ;
+
+ if(brightness <= 0 ||  brightness >=255)
+ {
+
+  fadeAmount = - fadeAmount ;
+ }
+  delay(30);
+}
+
